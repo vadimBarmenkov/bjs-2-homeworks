8e4e98570db02dae3dd78256be1d9fbe7811c425
vadimBarmenkov <vadimbarmenkov@gmail.com>
2022-08-13 00:20:58 +0300

    commit 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,23 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  if ( discriminant == 0){
+    arr = [-b / 2 * a];
+  }
+
+  if (discriminant > 0){
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+  }
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  percent = Number(percent / 100);
+  let S = Number(amount) * (1 + percent) - Number(contribution);
+
 
   // код для задачи №2 писать здесь
 
