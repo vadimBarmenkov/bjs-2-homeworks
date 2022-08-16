9eddabd71dceb9aaf83ea242bd4f19743114a371
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2022-08-16 15:06:13 +0300

    fixed errors

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,5 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   let discriminant = Math.pow(b, 2) - 4 * a * c;
 
   if ( discriminant == 0){
@@ -7,7 +7,7 @@ function solveEquation(a, b, c) {
   }
 
   if (discriminant > 0){
-    arr = [((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(2), ((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(2)];
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
   }
 
   return arr; // array
