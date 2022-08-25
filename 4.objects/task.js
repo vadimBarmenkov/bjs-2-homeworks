0f07b517e6ce1f7926eb07c8bcb46f1dfbda0866
Vadim Barmenkov <vadimbarmenkov@gmail.com>
2022-08-25 16:10:46 +0300

    task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,22 +1,40 @@
 function Student(name, gender, age) {
-    return{
-        name: name,
-        gender, gender,
-        age: age
-    };
+    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-
+    //добавление предмета
+    this.subject = subjectName;
 }
 
-Student.prototype.addMark = function (mark){
-    if(Student.mark){
-        mark: [mark];
-    }else{
-        
+Student.prototype.addMark = function (mark) {
+    if (!this.marks?.length) {
+        this.marks = [mark];
+        return;
     }
+    this.marks.push(mark);
+    // добавление оценок в массив
 }
 
 
+Student.prototype.addMarks = function (...marks) {
+    if (!this.marks?.length) {
+        this.marks = [...marks]
+        return
+    }
+    this.marks.push(...marks)
+}
+
+Student.prototype.getAverage = function (){
+    return this.marks.reduce(function (sum, element) {return sum+element;}, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason){
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
