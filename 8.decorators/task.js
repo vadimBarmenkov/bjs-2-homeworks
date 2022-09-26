01e876ca49fbd032a8e9dba6d58fce57d63d587b
vadimBarmenkov <vadimbarmenkov@gmail.com>
2022-09-26 13:15:47 +0300

    commit 1.2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,38 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+    return (...args) => {
+        const hash = args.join(",");
+        if (hash in cache){
+            return "Из кэша: " + cache[hash];
+        }
+
+        const result = func(...args);
+        if (cache.left === 5){
+            cache.shift();
+        }
+        cache[hash] = result;
+        return  "Вычисляем: " + result;
+    }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+    let timeOutId = null;
+    let count = 0;
+    let allCount;
+    return function (...args) {
+        if(timeOutId){
+            console.log("delete timeOutId");
+            count = 0;
+            clearTimeout(timeOutId);
+        }
+        console.log("create new timeOutId");
+        allCount++;
+        timeOutId = setTimeout(() => {
+            timeOutId = null;
+            count++;
+            console.log(func(...args));
+            console.log("run callback");
+        }, delay);
+    }
 }
